923f4c5d3a618836dd89c468a9053190443e441f
Baska Benjamin <benjamin.baska@gmail.com>
2022-09-19 13:43:56 +0200

    first push project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'D12_PRJ01',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
